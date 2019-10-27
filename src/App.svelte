9ac5a7915568c2c41b037b6c81d1c5d92c54d173
Tim Kim <timkim@qudocreative.com>
2019-10-27 06:44:31 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	let name = 'Lucas';
+</script>
+
+<style type="text/scss" scoped>
+	@import './assets/styles/base/variables';
+	@import './assets/styles/base/mixins';
+	
+		h1 {
+			&.testing {
+
+				color: $primary;
+				@include media-breakpoint-up (md) {
+					color: green;
+				}
+			}
+		}
+	
+		
+</style>
+<section>
+	<h1 class="testing">Hello {name}!</h1>
+</section>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import App from './App.svelte';
+import './assets/styles/bootstrap/bootstrap.scss';
+
+var app = new App({
+	target: document.body
+});
+
+export default app;
