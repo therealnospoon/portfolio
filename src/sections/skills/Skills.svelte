8e0e51c930ee6d98bac7ec26a82915df683a5f73
Tim Kim <timothy.kim@qudocreative.io>
2019-12-19 16:08:35 -0800

    working on icons in skills and transitions

--- src/sections/skills/Skills.svelte
+++ src/sections/skills/Skills.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import sal from "sal.js";
+  import skills from "./skills";
   import "sal.js/dist/sal.css";
 
   onMount(async () => {
@@ -11,46 +12,61 @@
   });
 
   const developerTools = [
-    "HTML, CSS, JS",
-    "Vue, React, Redux, Svelte",
-    "Node, VS Code",
-    "Firebase, Stripe",
-    "Gulp, Webpack, Rollup"
+    skills.bash,
+    skills.bootstrap,
+    skills.css,
+    skills.git,
+    skills.gulp,
+    skills.html,
+    skills.jquery,
+    skills.js,
+    skills.react,
+    skills.rollup,
+    skills.sass,
+    skills.svelte,
+    skills.vue,
+    skills.webpack,
+    skills.wordpress,
+    skills.npm,
+    skills.photoshop,
+    skills.figma,
+    skills.illustrator,
+    skills.node
   ];
-  const designerTools = [
-    "Figma",
-    "Adobe Illustrator",
-    "Sketch",
-    "Invision",
-    "Adobe Photoshop"
-  ];
+  // const designerTools = [
+  //   "Figma",
+  //   "Adobe Illustrator",
+  //   "Sketch",
+  //   "Invision",
+  //   "Adobe Photoshop"
+  // ];
 </script>
 
 <style type="text/scss">
   .section-wrapper {
     background-color: #f8fafc;
+    color: #454f5b;
   }
   .skills-box {
-    color: #454f5b;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
+    flex-wrap:wrap;
     align-items: center;
+    text-align: center;
+    margin: 3rem;
 
-    .skill-row {
-      justify-content: center;
+    // .skill-row {
+    //   justify-content: center;
+    // }
+
+    .skill-badge {
+      margin: 1rem;
     }
 
-    .skill-column {
-      text-align: center;
-      max-width: 320px;
-      margin: 3rem;
-      ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        li {
-          margin-top: 1rem;
-        }
+    .skill-icon {
+      img {
+        width: auto;
+        max-height: 55px;
       }
     }
   }
@@ -60,35 +76,35 @@
   <div class="section-wrapper">
     <div class="skills-container container">
       <div class="row justify-content-center align-items-center">
-        <h4 class="pt-5">Skills</h4>
-        <div class="skills-box col-12">
-          <div class="row skill-row w-100">
-            <div
-              class="skill-column col-12 col-md-6 d-flex flex-column
-              align-items-center"
-              data-sal="fade"
-              data-sal-easing="ease-in-out-quad"
-              data-sal-duration="500">
-              <i class="fas fa-code fa-2x mb-4" />
-              <h4 class="mb-3">Developer</h4>
-              <p>
+        <div class="col-12 d-flex flex-column align-items-center">
+          <i class="fas fa-code fa-2x mt-5 mb-3" />
+          <h4>Skills</h4>
+        </div>
+        <div
+          class="skills-box d-flex align-items-center col-12"
+          >
+
+          <!-- <p>
                 As a developer, I write clean, organized and performant code
-              </p>
-              <h5 class="mt-4">Development tools:</h5>
-              <ul>
-                {#each developerTools as tool, devToolIndex}
-                  <li
-                    class="mt-3"
-                    data-sal="fade"
-                    data-sal-easing="ease-in-out-quad"
-                    data-sal-duration="500"
-                    data-sal-delay={100 * devToolIndex}>
-                    <p>{tool}</p>
-                  </li>
-                {/each}
-              </ul>
-            </div>
+              </p> -->
+          <!-- <h5 class="mt-4">Development tools:</h5> -->
+          <!-- <ul> -->
+          {#each developerTools as tool, devToolIndex}
             <div
+              class="skill-badge mt-3"
+              data-sal="slide-right"
+              data-sal-easing="ease-in-out-quad"
+              data-sal-duration="250"
+              data-sal-delay={50 * devToolIndex}>
+              <span class="skill-icon">
+                <img src={tool.image} />
+              </span>
+              <p class="mt-1">{tool.name}</p>
+            </div>
+          {/each}
+          <!-- </ul> -->
+
+          <!-- <div
               class="skill-column col-12 col-md-6 d-flex flex-column
               align-items-center"
               data-sal="fade"
@@ -112,8 +128,8 @@
                   </li>
                 {/each}
               </ul>
-            </div>
-          </div>
+            </div> -->
+
         </div>
       </div>
     </div>
