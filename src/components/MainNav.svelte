79a352b28ab55ea5c67a633704ecf659ce5b8649
Tim Kim <timothy.kim@qudocreative.io>
2019-12-22 18:33:10 -0800

    added cool transition to hamburger menu

--- src/components/MainNav.svelte
+++ src/components/MainNav.svelte
@@ -1,15 +1,40 @@
 <script>
   import "sal.js/dist/sal.css";
+
+  let menuOpen = false;
+
+  function handleHamburger() {
+    const menu = document.getElementById("full-menu");
+    const inputCheck = document.getElementById("checkbox");
+    const bluecircle = document.getElementById("bluecircle");
+    if (menuOpen) {
+      inputCheck.checked = false;
+      menuOpen = false;
+
+      bluecircle.style.transform = "translateY(-100%)";
+      menu.style.opacity = 0;
+      menu.style.pointerEvents = "none";
+    } else {
+      inputCheck.checked = true;
+      menuOpen = true;
+      menu.style.opacity = 1;
+      menu.style.pointerEvents = "all";
+      bluecircle.style.transform = "translateY(-47%)";
+    }
+    console.log(menuOpen);
+  }
 </script>
 
 <style type="text/scss">
   .nav-container {
+    width: 100%;
     display: flex;
     justify-content: flex-end;
     align-items: center;
     position: fixed;
     top: 0;
     left: 0;
+    z-index: 5;
     overflow: hidden;
 
     .contact-link,
@@ -63,61 +88,53 @@
   }
 
   .full-menu {
-   transform: translate(100%, 100%);
-   transition: transform 500ms cubic-bezier(0.075, 0.82, 0.165, 1);
+    position: fixed;
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity 250ms ease-in 100ms;
     height: 100vh;
     width: 100vw;
-    background-color: #f8fafc;
+    background-color: #0069ed;
+    color: #f8fafc;
+    z-index: 3;
     .menu-contact,
     .menu-resume {
       display: inline-block;
       border: none;
       padding: 0.5rem 1rem;
       margin: 1rem;
-      width: 170px;
+      // width: 170px;
       text-decoration: none;
-      background: #0069ed;
       color: #f8fafc;
       border-radius: 2px;
       font-size: 1rem;
       cursor: pointer;
       text-align: center;
       transition: background 250ms ease-in-out, transform 150ms ease;
+      p {
+        position: relative;
+        &:after {
+          content: "";
+          width: 0;
+          height: 2px;
+          background-color: #f8fafc;
+          position: absolute;
+          top: 90%;
+          left: 0;
+          transition: width 250ms cubic-bezier(0.55, 0.055, 0.675, 0.19);
+        }
+      }
 
       &:hover,
       &:focus {
-        background: #0053ba;
-      }
-      &.menu-contact {
-        background: #f8fafc;
-        color: #0069ed;
-
         p {
-          position: relative;
           &:after {
-            content: "";
-            width: 0;
-            height: 2px;
-            background-color: #0069ed;
-            position: absolute;
-            top: 90%;
-            left: 0;
-            transition: width 250ms cubic-bezier(0.55, 0.055, 0.675, 0.19);
-          }
-        }
-
-        &:hover,
-        &:focus {
-          p {
-            &:after {
-              width: 100%;
-            }
+            width: 100%;
           }
         }
       }
     }
   }
-
 
   .hamburger {
     display: block;
@@ -126,7 +143,6 @@
     background: none;
     cursor: pointer;
     position: relative;
-    z-index: 1;
     -webkit-user-select: none;
     user-select: none;
 
@@ -149,9 +165,9 @@
       -webkit-touch-callout: none;
       &:checked ~ span {
         opacity: 1;
-        
+
         transform: rotate(-45deg) translate(0, -1px);
-        background: #232323;
+        background: #f8fafc;
       }
       &:checked ~ span:nth-last-child(2) {
         opacity: 0;
@@ -185,35 +201,43 @@
       }
     }
   }
+  .blue-circle {
+    width: 410vw;
+    height: 410vh;
+    border-radius: 30%;
+    position: fixed;
+    background-color: #0069ed;
+    transform: translateY(-100%);
+    transition: transform 400ms ease-in-out;
+  }
 </style>
 
 <section>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 nav-container">
-        <a href="mailto:timkimdesigns@gmail.com" class="contact-link">
-          <p class="m-0">Contact</p>
-        </a>
-        <a href="./images/TK_Dev_Resume.pdf" download class="resume-link">
-          <p class="m-0">Download Resume</p>
-        </a>
-        <button class="hamburger" type="button">
-          <input type="checkbox" />
-          <span />
-          <span />
-          <span />
-        </button>
-      </div>
-    </div>
+  <div class="nav-container">
+    <a href="mailto:timkimdesigns@gmail.com" class="contact-link">
+      <p class="m-0">Contact</p>
+    </a>
+    <a href="./images/TK_Dev_Resume.pdf" download class="resume-link">
+      <p class="m-0">Download Resume</p>
+    </a>
+    <button class="hamburger" type="button" on:click={handleHamburger}>
+      <input type="checkbox" id="checkbox" />
+      <span />
+      <span />
+      <span />
+    </button>
   </div>
-    <div class="full-menu d-flex flex-column justify-content-center align-items-center">
-     
-         <a href="mailto:timkimdesigns@gmail.com" class="menu-contact">
-          <p class="m-0">Contact</p>
-        </a>
-        <a href="./images/TK_Dev_Resume.pdf" download class="menu-resume">
-          <p class="m-0">Download Resume</p>
-        </a>
-    
-    </div>
+  <div
+    class="full-menu d-flex flex-column justify-content-center
+    align-items-center"
+    id="full-menu">
+    <a href="mailto:timkimdesigns@gmail.com" class="menu-contact">
+      <p class="m-0">Contact</p>
+    </a>
+    <a href="./images/TK_Dev_Resume.pdf" download class="menu-resume">
+      <p class="m-0">Download Resume</p>
+    </a>
+  </div>
+  <div class="blue-circle" id="bluecircle" />
+
 </section>
